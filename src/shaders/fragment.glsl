5b054f9db998277bc159692ab1a35bffceecbfbf
Movpasd <54367524+movpasd@users.noreply.github.com>
2026-08-16 21:26:19 +0100

    Cube given slightly prettier colours

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,5 +6,5 @@ layout(location = 0) in vec4 si_color;
 layout(location = 0) out vec4 ca_color;
 
 void main() {
-    ca_color = pow(si_color, vec4(0.33, 0.33, 0.33, 1.0));
+    ca_color = si_color;
 }
