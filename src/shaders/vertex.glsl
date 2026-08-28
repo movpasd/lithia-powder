555b8101319a4ed6e642d4f0d4600d4fcec4ff29
Movpasd <54367524+movpasd@users.noreply.github.com>
2026-08-28 18:26:13 +0100

    Simplify internal vertex shader logic

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -21,16 +21,38 @@ layout(location = 3) out vec3 so_worldNormal;
 const vec3 lightDir = normalize(vec3(-3.0, 0.0, 1.0));
 
 void main() {
-    vec4 worldPos = u_translation * u_rotation * va_position;
-    vec4 worldNormal = u_rotation * va_normal;
+    // vec4 uCamera_worldPosition = ;
+    mat4 uCamera_view = u_view;
+    mat4 uCamera_viewPerspective = u_persp * u_view;
+    vec4 uLamp_fromDirection = vec4(lightDir, 0.0);
+    mat4 uPose_transform = u_translation * u_rotation;
 
-    vec4 viewPos = u_view * worldPos;
-    gl_Position = u_persp * viewPos;
-    so_color = va_color;
+    vec4 vModelPosition = va_position;
+    vec4 vModelNormal = va_normal;
+    vec4 vColor = va_color;
+    vec4 sColor;
+    float sLampIllumination;
+    vec4 sWorldPosition;
+    vec4 sWorldNormal;
 
-    float lightDot = dot(normalize(worldNormal.xyz), lightDir);
-    so_lighting = (1.0 + lightDot) / 2.0;
+    // ---
 
-    so_worldPos = worldPos.xyz;
-    so_worldNormal = worldNormal.xyz;
+    sColor = vColor;
+
+    vec4 worldPosition = uPose_transform * vModelPosition;
+    vec4 worldNormal = uPose_transform * vModelNormal;
+    vec4 clipPosition = uCamera_viewPerspective * worldPosition;
+    gl_Position = clipPosition;
+    sWorldPosition = worldPosition;
+    sWorldNormal = worldNormal;
+
+    // nb: worldNormal and uLamp.dir assumed to be normalized and w=0
+    sLampIllumination = (1.0 + dot(worldNormal, uLamp_fromDirection)) / 2.0;
+
+    // ---
+
+    so_color = sColor;
+    so_lighting = sLampIllumination;
+    so_worldPos = sWorldPosition.xyz;
+    so_worldNormal = sWorldNormal.xyz;
 }
